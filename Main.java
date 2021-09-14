e391f5a48120674a3018311969bfabfe99179167
avi <90516887+Avinash098o@users.noreply.github.com>
2021-09-14 19:24:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.avi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i, num=5, sum=0;
+
+        for(i=0;i<=num;++i) {
+            sum+=i;
+        }
+        System.out.println("Sum of N numbers is " + sum);
+    }
+}
